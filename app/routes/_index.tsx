d0a7cb8200bcf6484259267fe2f0f336cc759e8c
Joshua Benfield <josh@benfield.org>
2023-06-07 23:38:51 -0400

    cleanup - branding

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,19 @@ import type { V2_MetaFunction } from '@remix-run/node';
 
 export const meta: V2_MetaFunction = () => {
     return [
-        { title: 'New Remix App' },
-        { name: 'description', content: 'Welcome to Remix!' },
+        { title: 'bambi' },
+        { name: 'description', content: 'A place to hold the passwords you hold deerly safe.' },
     ];
 };
 
 export default function Index() {
     return (
-        <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-            <h1>Welcome to Remix</h1>
-            <ul>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/tutorials/blog"
-                        rel="noreferrer"
-                    >
-                        15m Quickstart Blog Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/tutorials/jokes"
-                        rel="noreferrer"
-                    >
-                        Deep Dive Jokes App Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-                        Remix Docs
-                    </a>
-                </li>
-            </ul>
+        <div style={{ fontFamily: 'system-ui, sans-serif' }}>
+            <div>
+                <h1>bambi</h1>
+                <hr />
+                <p>A place to hold the passwords you hold deerly safe.</p>
+            </div>
         </div>
     );
 }
